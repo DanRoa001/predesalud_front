f6323c23ec98d7a9d046c92b05a444f1aee67fac
Daniel Roa <programador.id@comunidadincap.edu.co>
2025-05-26 10:09:38 -0500

    Ajustes de rutas para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { ToastContainer } from 'react-toastify'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/predesalud_front">
       <ToastContainer position='top-left'/>
       <App/>
     </BrowserRouter>,
